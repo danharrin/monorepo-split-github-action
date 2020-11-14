8349f2b4f2e25aa92e72fe99a620bb8746942859
Tomas Votruba <tomas.vot@gmail.com>
2020-11-14 14:41:24 +0000

    add note to separate internal API from behavior

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,17 +25,17 @@ git config --global user.name "$SPLIT_REPOSITORY_ORGANIZATION"
 git clone -- "https://$API_TOKEN_GITHUB@github.com/$SPLIT_REPOSITORY_ORGANIZATION/$SPLIT_REPOSITORY_NAME.git" "$CLONE_DIR"
 ls -la "$CLONE_DIR"
 
-echo "Cleaning destination repository of old files"
+echo "[Note] Cleaning destination repository of old files"
 # Copy files into the git and deletes all git
 find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
 ls -la "$CLONE_DIR"
 
-echo "Copying contents to git repo"
+echo "[Note] Copying contents to git repo"
 cp -r "$PACKAGE_DIRECTORY"/* "$CLONE_DIR"
 cd "$CLONE_DIR"
 ls -la
 
-echo "Adding git commit"
+echo "[Note] Adding git commit"
 ORIGIN_COMMIT="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
 
@@ -45,15 +45,15 @@ git status
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
-echo "Pushing git commit"
+echo "[Note] Pushing git commit"
 
 # --set-upstream: sets the branch when pushing to a branch that does not exist
-git push origin --set-upstream master
+git push --quiet origin master
 
 # push tag if present
 if [ ! -d "$TAG" ]
 then
-    echo "publishing $TAG"
+    echo "[Note] Publishing tag: $TAG"
 
     git tag $TAG -m "Publishing tag $TAG"
     git push --quiet origin $TAG
