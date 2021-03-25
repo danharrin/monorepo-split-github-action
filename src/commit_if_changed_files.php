62ceeff2eef14bd2e6c551e46706fb2b1e101121
TomasVotruba <tomas.vot@gmail.com>
2021-03-25 20:08:32 +0100

    output content with implode

--- src/commit_if_changed_files.php
+++ src/commit_if_changed_files.php
@@ -5,11 +5,13 @@ $envs = getenv();
 
 
 exec('git add .', $output);
-var_dump($output);
+$outputContent = implode(PHP_EOL, $output);
+echo $outputContent . PHP_EOL;
+
 
 exec('git status', $output);
-var_dump($output);
-
+$outputContent = implode(PHP_EOL, $output);
+echo $outputContent . PHP_EOL;
 
 // avoids doing the git commit failing if there are no changes to be commit, see https://stackoverflow.com/a/8123841/1348344
 exec('git diff-index --quiet HEAD', $output, $hasChangedFiles);
