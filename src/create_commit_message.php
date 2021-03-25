92e6b79ca454573f9113f9ca6b98f897c2df2d5c
TomasVotruba <tomas.vot@gmail.com>
2021-03-25 22:05:11 +0100

    use arg over env

--- src/create_commit_message.php
+++ src/create_commit_message.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 // WARNING! this must happen in the main directory repository, not the split one
 // if we do this in split repository, the original hash is missing there and it will fail
 
+$currentCommitHash = $argv[0];
+
 function createCommitMessage(string $commitSha): string
 {
     exec("git show -s --format=%B $commitSha", $output);
@@ -12,6 +14,4 @@ function createCommitMessage(string $commitSha): string
     return $bareMessage . PHP_EOL;
 }
 
-$envs = getenv();
-
-echo createCommitMessage($envs['GITHUB_SHA']);
+echo createCommitMessage($currentCommitHash);
