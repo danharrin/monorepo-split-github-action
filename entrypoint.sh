0536a68a296ef2f44e72e9614117959bdad65ebb
TomasVotruba <tomas.vot@gmail.com>
2021-03-25 13:52:03 +0100

    remove git status, already duplicates

--- entrypoint.sh
+++ entrypoint.sh
@@ -103,7 +103,6 @@ ls -la
 note "Adding git commit"
 
 git add .
-git status
 
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
