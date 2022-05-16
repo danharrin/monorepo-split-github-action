c626465afa13d2ca69c29d6897632d73747891e1
Maxim Smakouz <m.smakouz@gmail.com>
2022-05-16 17:13:08 +0300

    Fix

--- entrypoint.php
+++ entrypoint.php
@@ -92,7 +92,7 @@ if ($changedFiles) {
     note($message);
 
     exec_with_note("git commit --message '$commitMessage'");
-    exec_with_note('git pull');
+    exec_with_note('git pull origin ' . $config->getBranch());
     exec_with_note('git push --quiet origin ' . $config->getBranch());
 } else {
     note('No files to change');
@@ -107,7 +107,7 @@ if ($config->getTag()) {
     $commandLine = sprintf('git tag %s -m "%s"', $config->getTag(), $message);
     exec_with_note($commandLine);
 
-    exec_with_note('git pull');
+    exec_with_note('git pull origin ' . $config->getBranch());
     exec_with_note('git push --quiet origin ' . $config->getTag());
 }
 
