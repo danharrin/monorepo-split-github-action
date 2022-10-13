889abd9a74e4f85be761597af25a4f7f84daaf13
Tomas Votruba <tomas.vot@gmail.com>
2022-10-13 16:22:19 +0200

    Revert "Fail pipeline when git push errors (#34)" (#38)
    This reverts commit f258435648230016099affdb0d09eed044e24e43.

--- entrypoint.php
+++ entrypoint.php
@@ -115,12 +115,7 @@ if ($changedFiles) {
     note($message);
 
     exec("git commit --message '{$commitMessage}'");
-    exec('git push --quiet origin ' . $config->getBranch(), $outputLines, $exitCode);
-
-    if ($exitCode > 0) {
-        error('Failed to push changes!');
-        exit($exitCode);
-    }
+    exec('git push --quiet origin ' . $config->getBranch());
 } else {
     note('No files to change');
 }
